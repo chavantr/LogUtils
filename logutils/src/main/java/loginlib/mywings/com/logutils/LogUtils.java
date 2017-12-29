3b6fc5ff3067bb3330b2214a21ff06167b85d4e6
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2017-12-29 14:03:43 +0530

    Initial commit

---
+++ logutils/src/main/java/loginlib/mywings/com/logutils/LogUtils.java
@@ -0,0 +1,13 @@
+package loginlib.mywings.com.logutils;
+
+
+import android.util.Log;
+
+public class LogUtils {
+
+    private static final String LOG = "LogUtils";
+
+    public static void d(String log) {
+        Log.d(LOG, log);
+    }
+}
